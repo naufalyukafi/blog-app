890bdbee5eebfc3af2bc564f3c82486af6e2a998
naufalyukafi <naufalyukafi10@gmail.com>
2024-06-01 15:37:42 +0700

    fix: fixed error build

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,8 +7,8 @@ const NotFound = () => {
             <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                 <div className="mx-auto max-w-screen-sm text-center">
                     <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
-                    <p className="mb-4 text-3xl tracking-tight font-bold ">Something's missing.</p>
-                    <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
+                    <p className="mb-4 text-3xl tracking-tight font-bold ">Something missing.</p>
+                    <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we cant find that page. Youll find lots to explore on the home page. </p>
                     <Link href="/">Back Home</Link>
                 </div>
             </div>
