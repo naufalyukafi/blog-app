16fe87c91e4de05b698c29db8efea921260a97ef
naufalyukafi <naufalyukafi10@gmail.com>
2024-06-02 01:02:59 +0700

    fix: fix first page article service

--- src/services/article/index.ts
+++ src/services/article/index.ts
@@ -2,8 +2,9 @@ import apiBlogApp from "@/utils/apiBlogApp";
 
 
 const articleService = {
-    fetchArticleList(page: string = '1') {
-        return apiBlogApp.get(`/posts?page=${page}&per_page=12`);
+    fetchArticleList(page: string) {
+        const currentPage = page ?? 1
+        return apiBlogApp.get(`/posts?page=${currentPage}&per_page=12`);
     },
 
 }
